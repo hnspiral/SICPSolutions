3390fc3878ceb2f4e8dcfe98a639a77048cbb015
hnspiral <nguyentuongbaohanh@gmail.com>
2020-04-22 13:48:54 -0700

    Ex1.04

---
+++ Chapter1/Ex1.04.scm
@@ -0,0 +1,6 @@
+;; to explain the procedure a plus absolute b
+;; whereby operators are compound expressions
+;; depending on the predicate (> b 0)
+;; if b > 0, the operator is "+"
+;; otherwise the operator is "-"
+;; finally the opertor is applied on the 2 operands a and b

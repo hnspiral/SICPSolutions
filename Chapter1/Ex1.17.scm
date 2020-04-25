febdfbe85c545520888acb72a763e517966124e9
hnspiral <nguyentuongbaohanh@gmail.com>
2020-04-25 14:03:23 -0700

    Ex1.17

---
+++ Chapter1/Ex1.17.scm
@@ -0,0 +1,11 @@
+;;; Define multiplcation procedure that uses logarithmic number of steps
+(define (double x) (+ x x))
+(define (halve x) (/ x 2))
+(define (is-even? x) (= (remainder x 2) 0))
+
+(define (multi a b)
+  (cond ((= b 1) a)
+		((is-even? b)
+		 (double (multi a (halve b))))
+		(else
+		  (+ a (multi a (- b 1))))))

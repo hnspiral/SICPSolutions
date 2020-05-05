c226ac0eaf2a846da69115e49455d48e4b7abdf7
hnspiral <nguyentuongbaohanh@gmail.com>
2020-05-05 15:49:16 -0700

    Ex1.37

---
+++ Chapter1/Ex1.37.scm
@@ -0,0 +1,19 @@
+;;;a. Define cont-frac procedure
+(define (cont-frac n d k)
+  (define (recur i)
+  (if (> i k)
+	  0
+	  (/ (n i) (+ (d i) (recur (+ i 1))))))
+  (recur 1.0))
+
+;;; Test cont-frac with n = 1 and d = 1
+(cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 1000)
+Value: .6180339887498948
+
+;;;b. Define cont-frac as iterative process
+(define (cont-frac n d k)
+  (define (iter i result)
+	(if (= i 1)
+	    result
+		(iter (- i 1) (/ (n i) (+ (d i) result)))))
+  (iter k 0))

083094f3d772cf5bc8bee49da0d8396ee213999b
hnspiral <nguyentuongbaohanh@gmail.com>
2020-05-05 11:19:32 -0700

    Ex1.34

---
+++ Chapter1/Ex1.34.scm
@@ -0,0 +1,6 @@
+;;; if we call (f f) there would be error: object 2 is not applicable
+;;; specify a procedure to use in its place
+;;; (f f)
+;;; (f 2)
+;;; (2 2)
+;;; The operator is 2, which is an "invalid" operator by Scheme interpreter

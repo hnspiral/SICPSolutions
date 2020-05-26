70da67bfcdcd41310767284880bb029da270e6d4
hnspiral <nguyentuongbaohanh@gmail.com>
2020-05-25 22:07:51 -0700

    Ex2.61

---
+++ Chapter2/Ex2.61.scm
@@ -0,0 +1,13 @@
+;;; Give an implementation of adjoin-set using the ordered representation.
+;;; By analogy with element-of-set? show how to take advantage of the ordering
+;;; to produce a procedure that requires on the average about half
+;;; as many steps as with the ordered representation
+(define (adjoin-set x set)
+  (cond ((null? set)
+		 (cons x '()))
+	    ((< x (car set))
+	     (cons x set))
+	    ((= x (car set))
+		 set)
+		((> x (car set))
+		 (adjoin-set x (cdr set)))))
